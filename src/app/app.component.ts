5b4cf3e2d890ddb0eab601daa75e475d79eff679
Nasreen <shaikhnasrin88@gmail.com>
2023-05-24 16:49:55 +0530

    I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Training_Week1';
+
+
+  filterForm: FormGroup | null = null;;
+  students = ['John Doe', 'Jane Smith', 'Alice Johnson', 'Bob Anderson'];
+  filteredStudents$: Observable<string[]> | undefined;
+
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.filterForm = this.formBuilder.group({
+      filterText: ['']
+    });
+
+    this.filteredStudents$ = this.filterForm.get('filterText')?.valueChanges.pipe(
+      startWith(''),
+      map(filterText => this.filterStudents(filterText))
+    );
+  }
+
+  filterStudents(filterText: string): string[] {
+    if (!filterText) {
+      return this.students;
+    }
+
+    filterText = filterText.toLowerCase();
+    return this.students.filter(student => student.toLowerCase().includes(filterText));
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { NumberOnlyDirective } from './number-only.directive';
+import { StudentFilterPipe } from './student-filter.pipe';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { StudentListComponent } from './student-list/student-list.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './parent/child/child.component';
+import { DataService } from './data.service'
+import { ServiceDataComponent } from './service-data/service-data.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { ExampleComponent } from './reactive-form-validation/reactive-form-validation.component';
+import { TemplateFormComponent } from './template-driven-form/template-driven-form.component';
+import { FormArrayComponent } from './form-array/form-array.component';
+import { ReactFormComponent } from './react-form/react-form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivesComponent,
+    NumberOnlyDirective,
+    StudentFilterPipe,
+    Child1Component,
+    Child2Component,
+    StudentListComponent,
+    ParentComponent,
+    ChildComponent,
+    ServiceDataComponent,
+    ReactiveFormComponent,
+    TemplateFormComponent,
+    ExampleComponent,
+    FormArrayComponent,
+    ReactFormComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
