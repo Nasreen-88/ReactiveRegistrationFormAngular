ad04d828795844cc9edecde7c01246ce122cee00
Nasreen <shaikhnasrin88@gmail.com>
2023-05-30 15:11:30 +0530

    commit for alert

---
+++ src/app/alert/alert.component.ts
@@ -0,0 +1,44 @@
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'ngx-alert',
+//   templateUrl: './alert.component.html',
+//   styleUrls: ['./alert.component.css']
+// })
+// export class AlertComponent {
+//   @Input()
+//   alertType!: string; // Alert type: 'alert-success', 'alert-info', 'alert-warning', 'alert-danger'
+//   @Input() dismissible: boolean = true; // Whether the alert is dismissible
+//   showAlert: boolean = true;
+
+//   closeAlert(): void {
+//     this.showAlert = false;
+//   }
+// }
+
+
+// For DatePicker
+
+import { Component } from '@angular/core';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
+@Component({
+  selector: 'ngx-alert',
+templateUrl: './alert.component.html',
+styleUrls: ['./alert.component.css']
+})
+export class AlertComponent {
+  datepickerConfig: Partial<BsDatepickerConfig>;
+
+  constructor() {
+    this.datepickerConfig = {
+      containerClass: 'theme-default',
+      showWeekNumbers: false
+    };
+  }
+
+  handleDateChange(date: Date): void {
+    console.log('Selected date:', date);
+  }
+}
+
